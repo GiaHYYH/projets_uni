41eb6e71eb153af37e1f4d4ec1cf6f6725e6c8cb
G. Eraste <155081101+GiaHYYH@users.noreply.github.com>
2026-09-15 13:31:20 +0200

    Update script_index.js

--- projet.pokemon/js/script_index.js
+++ projet.pokemon/js/script_index.js
@@ -22,7 +22,7 @@ function loadXMLDoc() {
             showPageLinks();
         }
     };
-    xmlhttp.open("GET", "https://obiwan.univ-brest.fr/~e22403362/data/bdd_GE.xml", true);
+    xmlhttp.open("GET", "https://obiwan.univ-brest.fr/~exxxxxxxx/data/bdd_GE.xml", true);
     xmlhttp.send();
 }
 
