151894d20f13773158d71a7fb63244bb97e33ac1
G. Eraste <155081101+GiaHYYH@users.noreply.github.com>
2026-09-15 13:30:49 +0200

    Update script_apprendre.js

--- projet.pokemon/js/script_apprendre.js
+++ projet.pokemon/js/script_apprendre.js
@@ -20,7 +20,7 @@ function loadXMLDoc() {
             showPageLinks();
         }
     };
-    xmlhttp.open("GET", "https://obiwan.univ-brest.fr/~e22403362/data/bdd_GE.xml", true);
+    xmlhttp.open("GET", "https://obiwan.univ-brest.fr/~exxxxxxxx/data/bdd_GE.xml", true);
     xmlhttp.send();
 }
 
